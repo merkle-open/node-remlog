2f65dfe24ee2ff64bb52f1e7d9a5d90654093a6f
Jan R. Biasi <janbiasi@users.noreply.github.com>
2018-01-15 22:07:17 +0100

    Create browser client boilerplate

---
+++ packages/browser-client/index.js
@@ -0,0 +1,46 @@
+const { ConsoleLogger } = require('@remlog/log');
+const { Scheme } = require('@remlog/scheme');
+const pkg = require('./package.json');
+
+const logger = new ConsoleLogger('BrowserClient');
+
+const defaultConfig = {
+  host: '',
+  port: '',
+};
+
+class BrowserClient {
+  constructor(config = defaultConfig) {
+    this.config = config;
+  }
+  
+  getScheme(data = {}) {
+    return new Scheme({
+      version: pkg.version,
+      host: 'tbd',
+      shortMessage: '',
+      fullMessage: data.fullMessage, 
+      timestamp: new Date().toISOString(),
+      level: data.level,
+      facility: data.facility,
+      line: data.line,
+      file: data.file
+    });
+  }
+  
+  send(data = {}) {
+    const img = document.createElement("img");
+    const payload = this.getScheme(data).serialize();
+    
+    img.src = `${this.config.host}:${this.config.port}/trace.jpg?payload=${payload}`;
+    img.width = 0;
+    img.height = 0;
+    
+    document.body.appendChild(img);
+  }
+}
+
+exports = module.exports = {
+  BrowserClient,
+  defaultConfig,
+};
