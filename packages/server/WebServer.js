55706b465428b150e82970e50a763c26b4d6c493
Jan R. Biasi <janbiasi@users.noreply.github.com>
2018-01-15 22:37:58 +0100

    Update WebServer.js

--- packages/server/WebServer.js
+++ packages/server/WebServer.js
@@ -1,6 +1,9 @@
 const restify = require('restify');
 const { ConsoleLogger } = require('@remlog/log');
 
+const { ConsoleLogger } = require('@remlog/log');
+const pkg = require('./package.json');
+
 const defaultConfig = {
     port: 8189,
 };
@@ -12,6 +15,37 @@ class WebServer {
         this.config = config;
         this.instance = restify.createServer();
     }
+    
+    attachRouter() {
+        this.instance.use(restify.plugins.throttle({
+            burst: 10,  // max 10 concurrent requests (if tokens)
+            rate: 0.5,  // steady state: 1 request / 2 seconds
+            ip: true,   // throttle per IP address
+        }));
+        
+        /**
+         * TODO: Add middlewares:
+         * - restify-cors-middleware
+         * - 
+         */
+        this.instance
+            .pre((req, res, next) => {
+                res.charSet('utf-8');
+                res.setHeader('X-RemLog-Inflight', this.instance.inflightRequests);
+                res.setHeader('X-RemLog-Version', pkg.version);
+            })
+            .get('/logs', (req, res, next) => {
+                // TODO: Return all logs in an array
+                next();
+            })
+            .get('/trace.jpg', (req, res, next) => {
+                // TODO: Read file from fs and send it via res.sendRaw()
+                next();
+            })
+            .post('/trace', (req, res, next) => {
+                // TODO: Implement ajax mechanism
+                next();
+            });
 
     start() {
         const { port } = this.config;
