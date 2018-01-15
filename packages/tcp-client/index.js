e4c6e5dd6b4704336c4591f683f6cd7f93a0fee0
Jan Biasi <jan.biasi@namics.com>
2018-01-15 17:17:19 +0100

    Add comments

--- packages/tcp-client/index.js
+++ packages/tcp-client/index.js
@@ -3,9 +3,20 @@ const { ConsoleLogger } = require('@remlog/log');
 
 const logger = new ConsoleLogger('TCPClient');
 
+const clients = [];
+
+/**
+ * tbd.
+ * @param {*} config
+ * @param {*} handler
+ * @returns {<net.Socket>}
+ */
 const connect = (config = {}, handler = () => {}) => {
     const { host, port } = config;
     const client = new Socket();
+
+    // Keep track of the clients
+    clients.push(client);
 
     client.connect(port, host, () => {
         logger.success(`Connected to ${host}:${port} established`);
@@ -18,8 +29,11 @@ const connect = (config = {}, handler = () => {}) => {
     client.on('close', () => {
         logger.success(`Connection to ${host}:${port} closed`);
     });
+
+    return client;
 };
 
 exports = module.exports = {
     connect,
+    clients,
 };
