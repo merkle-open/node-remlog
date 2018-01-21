c892e55cd518c346a5ab1f4d2b6436bc34cef5e2
Jan Biasi <biasijan@gmail.com>
2018-01-21 23:30:02 +0100

    fix(transports): Update transport ID to fit package name convention
    BREAKING CHANGE: CLI uses new transport IDs

--- packages/transports/Console/index.js
+++ packages/transports/Console/index.js
@@ -1,21 +1,21 @@
-const chalk = require('chalk');
-const Transport = require('../Transport');
-const { getLogLevelName } = require('@remlog/utils');
+const chalk = require("chalk");
+const Transport = require("../Transport");
+const { getLogLevelName } = require("@remlog/utils");
 
-const TRANSPORT_ID = '@remlog/transports/Console';
+const TRANSPORT_ID = "@namics/remlog-transports/Console";
 
 class ConsoleTransport extends Transport {
-    trace(payload, finish) {
-        console.log(
-            [
-                `[${payload.timestamp}]`,
-                `${getLogLevelName(payload.level).toUpperCase()} -`,
-                `${payload.shortMessage} (${payload.id})`,
-            ].join(' ')
-        );
+  trace(payload, finish) {
+    console.log(
+      [
+        `[${payload.timestamp}]`,
+        `${getLogLevelName(payload.level).toUpperCase()} -`,
+        `${payload.shortMessage} (${payload.id})`
+      ].join(" ")
+    );
 
-        finish();
-    }
+    finish();
+  }
 }
 
 ConsoleTransport.id = TRANSPORT_ID;
--- packages/transports/Transport.js
+++ packages/transports/Transport.js
@@ -1,81 +1,86 @@
-const path = require('path');
-const { Scheme } = require('@remlog/scheme');
-const { Logger } = require('@remlog/debug');
-const { createStream, checkReadWritePermission } = require('./FileSystem/stream');
+const path = require("path");
+const { Scheme } = require("@remlog/scheme");
+const { Logger } = require("@remlog/debug");
+const {
+  createStream,
+  checkReadWritePermission
+} = require("./FileSystem/stream");
 
-const TRANSPORT_ID = '@remlog/transports/Transport';
-const GENERIC_TRANSPORT_LOGFILE = path.resolve(process.cwd(), 'remlog.json');
+const TRANSPORT_ID = "@namics/remlog-transports/Transport";
+const GENERIC_TRANSPORT_LOGFILE = path.resolve(process.cwd(), "remlog.json");
 
 const initialPayload = {
-    $empty: true,
+  $empty: true
 };
 
 class Transport {
-    constructor() {
-        this.logger = new Logger(`Transport(${this.constructor.name})`);
+  constructor() {
+    this.logger = new Logger(`Transport(${this.constructor.name})`);
+  }
+
+  /**
+   * Validates a payload against the scheme definition
+   * @param {Object?} payload
+   * @returns {<@remlog/scheme.Scheme>|boolean}
+   */
+  validate(payload = initialPayload) {
+    const scheme = new Scheme(payload);
+    const validation = scheme.validate();
+
+    let error = [];
+
+    if (validation.length >= 0) {
+      validation.forEach((validationItem, index) => {
+        this.logger.error(`[#${index + 1}] ${validationItem.error.message}`);
+      });
+
+      error = validation;
     }
 
-    /**
-     * Validates a payload against the scheme definition
-     * @param {Object?} payload
-     * @returns {<@remlog/scheme.Scheme>|boolean}
-     */
-    validate(payload = initialPayload) {
-        const scheme = new Scheme(payload);
-        const validation = scheme.validate();
+    return {
+      valid: !!(validation.length === 0),
+      error: error,
+      scheme
+    };
+  }
 
-        let error = [];
+  /**
+   * @abstract
+   * @param {Object?} payload
+   */
+  trace(payload = {}) {
+    this.logger.error(
+      `${this.constructor.name} does not implement the method .trace()`
+    );
+  }
 
-        if (validation.length >= 0) {
-            validation.forEach((validationItem, index) => {
-                this.logger.error(`[#${index + 1}] ${validationItem.error.message}`);
-            });
-
-            error = validation;
-        }
-
-        return {
-            valid: !!(validation.length === 0),
-            error: error,
-            scheme,
-        };
-    }
-
-    /**
-     * @abstract
-     * @param {Object?} payload
-     */
-    trace(payload = {}) {
-        this.logger.error(`${this.constructor.name} does not implement the method .trace()`);
-    }
-
-    /**
-     * Save the current trace to the internal logfile
-     * @param {Object?} payload
-     */
-    saveTraceToLock(payload = {}, logfile = GENERIC_TRANSPORT_LOGFILE) {
-        new Promise((resolve, reject) => {
-            checkReadWritePermission(logfile)
-                .then(() => {
-                    resolve();
-                })
-                .catch(() => {
-                    const stream = createStream(logfile);
-                    stream.write('[]');
-                    resolve();
-                });
+  /**
+   * Save the current trace to the internal logfile
+   * @param {Object?} payload
+   */
+  saveTraceToLock(payload = {}, logfile = GENERIC_TRANSPORT_LOGFILE) {
+    new Promise((resolve, reject) => {
+      checkReadWritePermission(logfile)
+        .then(() => {
+          resolve();
         })
-            .then(() => {
-                const contents = require(logfile);
-                contents.push(payload);
+        .catch(() => {
+          const stream = createStream(logfile);
+          stream.write("[]");
+          resolve();
+        });
+    })
+      .then(() => {
+        const contents = require(logfile);
+        contents.push(payload);
 
-                const stream = createStream(logfile);
-                stream.write(JSON.stringify(contents));
-            })
-            .catch(e => {
-                this.logger.error(e.message);
-            });
-    }
+        const stream = createStream(logfile);
+        stream.write(JSON.stringify(contents));
+      })
+      .catch(e => {
+        this.logger.error(e.message);
+      });
+  }
 }
 
 Transport.id = TRANSPORT_ID;
