637387040c2c629d4b8f107b57f9de79b984d8e9
Jan Biasi <jan.biasi@namics.com>
2018-01-23 10:00:00 +0100

    style(transports): Update code style of transports

--- packages/transports/Console/index.js
+++ packages/transports/Console/index.js
@@ -1,21 +1,26 @@
-const chalk = require("chalk");
-const Transport = require("../Transport");
-const { getLogLevelName } = require("@namics/remlog-utils");
+const { gray, magenta } = require('chalk');
+const Transport = require('../Transport');
+const { getLogLevelName } = require('@namics/remlog-utils');
 
-const TRANSPORT_ID = "@namics/remlog-transports/Console";
+const TRANSPORT_ID = '@namics/remlog-transports/Console';
 
 class ConsoleTransport extends Transport {
-  trace(payload, finish) {
-    console.log(
-      [
-        `[${payload.timestamp}]`,
-        `${getLogLevelName(payload.level).toUpperCase()} -`,
-        `${payload.shortMessage} (${payload.id})`
-      ].join(" ")
-    );
+	trace(payload, finish) {
+		if (!console || !console.log) {
+			throw new Error(`Console is not available in your environement.`);
+		}
 
-    finish();
-  }
+		console.log(
+			[
+				gray(`[${payload.timestamp}]`),
+				`${getLogLevelName(payload.level).toUpperCase()} -`,
+				`${payload.shortMessage}`,
+				magenta(`(${payload.id})`)
+			].join(' ')
+		);
+
+		finish();
+	}
 }
 
 ConsoleTransport.id = TRANSPORT_ID;
--- packages/transports/FileSystem/index.js
+++ packages/transports/FileSystem/index.js
@@ -1,36 +1,36 @@
-const fs = require("fs");
-const Transport = require("../Transport");
-const path = require("path");
-const { getLogLevelName } = require("@namics/remlog-utils");
-const { createIntelligentStream } = require("./stream");
+const fs = require('fs');
+const Transport = require('../Transport');
+const path = require('path');
+const { getLogLevelName } = require('@namics/remlog-utils');
+const { createIntelligentStream } = require('./stream');
 
 const CWD = path.resolve(process.cwd());
-const PACKAGE_PATH = path.resolve(__dirname, "..");
+const PACKAGE_PATH = path.resolve(__dirname, '..');
 
-const TRANSPORT_ID = "@namics/remlog-transports/FileSystem";
-const TRANSPORT_LOGFILE = path.resolve(process.cwd(), "remlog.log");
+const TRANSPORT_ID = '@namics/remlog-transports/FileSystem';
+const TRANSPORT_LOGFILE = path.resolve(process.cwd(), 'remlog.log');
 const LOGFILE_DELIMITER = `\r\n`;
 
 class FileSystemTransport extends Transport {
-  createMessage(payload) {
-    return [
-      `[${payload.timestamp}]`,
-      `${getLogLevelName(payload.level).toUpperCase()}`,
-      `- ${payload.shortMessage}`,
-      `(#${payload.id})`,
-      LOGFILE_DELIMITER
-    ].join(" ");
-  }
+	createMessage(payload) {
+		return [
+			`[${payload.timestamp}]`,
+			`${getLogLevelName(payload.level).toUpperCase()}`,
+			`- ${payload.shortMessage}`,
+			`(#${payload.id})`,
+			LOGFILE_DELIMITER
+		].join(' ');
+	}
 
-  trace(payload, finish) {
-    const validation = this.validate(payload);
+	trace(payload, finish) {
+		const validation = this.validate(payload);
 
-    createIntelligentStream(TRANSPORT_LOGFILE).then(stream => {
-      stream.write(this.createMessage(payload));
-    });
+		createIntelligentStream(TRANSPORT_LOGFILE).then(stream => {
+			stream.write(this.createMessage(payload));
+		});
 
-    finish();
-  }
+		finish();
+	}
 }
 
 FileSystemTransport.id = TRANSPORT_ID;
