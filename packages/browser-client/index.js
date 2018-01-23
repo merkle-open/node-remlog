6fc19c10358ce38fdbb5ad906afd82877e7eb4ee
Jan Biasi <jan.biasi@namics.com>
2018-01-23 13:13:07 +0100

    fix(browser-client): Use Node import before webpack build

--- packages/browser-client/index.js
+++ packages/browser-client/index.js
@@ -1,50 +1,50 @@
-import { version } from "./package.json";
-import { getTracerImageUrl } from "@namics/remlog-utils";
+import { version } from './package.json';
+import { getTracerImageUrl } from '@namics/remlog-utils/src';
 
 const defaultConfig = {
-  host: "0.0.0.0",
-  port: "80"
+	host: '0.0.0.0',
+	port: '80'
 };
 
 class BrowserClient {
-  constructor(config = defaultConfig) {
-    this.config = config;
-  }
+	constructor(config = defaultConfig) {
+		this.config = config;
+	}
 
-  getScheme(data = {}) {
-    return {
-      version,
-      host: window.location.host,
-      client: "BrowserClient",
-      userAgent: navigator.userAgent,
-      shortMessage: data.shortMessage,
-      fullMessage: data.fullMessage,
-      level: data.level || 0,
-      facility: data.facility,
-      line: data.line,
-      file: data.file
-    };
-  }
+	getScheme(data = {}) {
+		return {
+			version,
+			host: window.location.host,
+			client: 'BrowserClient',
+			userAgent: navigator.userAgent,
+			shortMessage: data.shortMessage,
+			fullMessage: data.fullMessage,
+			level: data.level || 0,
+			facility: data.facility,
+			line: data.line,
+			file: data.file
+		};
+	}
 
-  send(message, data = {}) {
-    if (typeof message === "string") {
-      data.shortMessage = message;
-    } else {
-      data = message || {};
-    }
+	send(message, data = {}) {
+		if (typeof message === 'string') {
+			data.shortMessage = message;
+		} else {
+			data = message || {};
+		}
 
-    const img = document.createElement("img");
-    const payload = this.getScheme(data);
+		const img = document.createElement('img');
+		const payload = this.getScheme(data);
 
-    img.src = getTracerImageUrl(this.config, payload);
-    img.width = 0;
-    img.height = 0;
+		img.src = getTracerImageUrl(this.config, payload);
+		img.width = 0;
+		img.height = 0;
 
-    document.body.appendChild(img);
-  }
+		document.body.appendChild(img);
+	}
 }
 
 exports = module.exports = {
-  BrowserClient,
-  defaultConfig
+	BrowserClient,
+	defaultConfig
 };
