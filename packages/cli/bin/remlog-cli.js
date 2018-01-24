ca6a84af49a37390228d274fc0d0d4ac0580e402
Jan Biasi <jan.biasi@namics.com>
2018-01-24 13:34:34 +0100

    feat(cli): Enable SSL param support and new cct (create-custom-transport) command

--- packages/cli/bin/remlog-cli.js
+++ packages/cli/bin/remlog-cli.js
@@ -1,59 +1,101 @@
 #!/usr/bin/env node
-
+const path = require('path');
 const program = require('commander');
+const { red, green } = require('chalk');
 const pkg = require('../package.json');
-const { start, request } = require('../index');
-
+const { TemplatePaths, start, request, copyTemplate } = require('../index');
+const CMD_DIR = path.resolve(process.cwd());
 program.version(`${pkg.version}`);
 
 program
-    .command('server')
-    .option('-p, --port <port>', 'The targeted port of the server instance')
-    .option(
-        '-t, --transport <transport>',
-        'Select a custom transport for the server, default: Console'
-    )
-    .option('-c, --cors <whitelist>', 'Set a custom whitelist for CORS requests')
-    .action(command => {
-        return start(command.port, command.transport, command.cors);
-    });
+	.command('server')
+	.option('-p, --port <port>', 'The targeted port of the server instance')
+	.option(
+		'-t, --transport <transport>',
+		'Select a custom transport for the server, default: Console'
+	)
+	.option('-c, --cors <whitelist>', 'Set a custom whitelist for CORS requests')
+	.option('-s, --secure', 'Enable SSL, requires option <ssl-cert> and <ss-key>')
+	.option('-k, --ssl-key <ssl-key>', 'Path to the SSL key')
+	.option('-e, --ssl-cert <ssl-cert>', 'Path to the SSL certificate')
+	.option('-r, --ssl-passphrase <ssl-passphrase>', 'Optional: SSL passphrase if set')
+	.action(command => {
+		return start(
+			command.port,
+			command.transport,
+			command.cors,
+			command.secure
+				? {
+						key: path.join(CMD_DIR, command.sslKey || 'unknown.key'),
+						cert: path.join(CMD_DIR, command.sslCert || 'unknown.cert'),
+						passphrase: command.sslPassphrase
+					}
+				: null
+		);
+	});
 
 program
-    .command('trace <message>')
-    .option('-s, --secure', 'Whether to make a HTTPS or HTTP response')
-    .option('-h, --host <host>', 'Target host')
-    .option('-p, --port <port>', 'Target host port')
-    .action((message, command) => {
-        return request(command.host, command.port, command.secure, message);
-    });
+	.command('cct')
+	.option('-n, --name <transport>', 'The name of the custom transport')
+	.option('-t, --target <dest>', 'Target of the custom transport class')
+	.action(command => {
+		const { name, target } = command;
+
+		if (!name || !target) {
+			console.log(red(`Missing name or target argument`));
+			return null;
+		}
+
+		const TransportName = `${name[0].toUpperCase()}${name.substring(1)}`;
+		const targetFile = path.join(target, `Remlog${TransportName}Transport.js`);
+
+		copyTemplate(TemplatePaths.Transport, targetFile, {
+			name: TransportName
+		})
+			.then(() => {
+				console.log(`Saved custom transport in ${targetFile}`);
+			})
+			.catch(e => {
+				console.error(`Failed writing Transport to ${targetFile}: ${e.message}`);
+			});
+	});
 
 program
-    .command('ping')
-    .option('-s, --secure', 'Whether to make a HTTPS or HTTP response')
-    .option('-h, --host <host>', 'Target host')
-    .option('-p, --port <port>', 'Target host port')
-    .action(command => {
-        return request(
-            command.host,
-            command.port,
-            command.secure,
-            JSON.stringify({
-                shortMessage: 'Ping from command line interface',
-                level: 1,
-                line: 32,
-                client: 'CLI'
-            })
-        );
-    });
+	.command('trace <message>')
+	.option('-s, --secure', 'Whether to make a HTTPS or HTTP response')
+	.option('-h, --host <host>', 'Target host')
+	.option('-p, --port <port>', 'Target host port')
+	.action((message, command) => {
+		return request(command.host, command.port, command.secure, message);
+	});
+
+program
+	.command('ping')
+	.option('-s, --secure', 'Whether to make a HTTPS or HTTP response')
+	.option('-h, --host <host>', 'Target host')
+	.option('-p, --port <port>', 'Target host port')
+	.action(command => {
+		return request(
+			command.host,
+			command.port,
+			command.secure,
+			JSON.stringify({
+				shortMessage: 'Ping from command line interface',
+				level: 1,
+				line: 32,
+				client: 'CLI'
+			})
+		);
+	});
 
 if (!process.argv.slice(2).length) {
-    program.outputHelp();
+	program.outputHelp();
 }
 
 ['SIGINT', 'SIGTERM'].forEach(sig => {
-    process.on(sig, () => {
-        process.exit();
-    });
+	process.on(sig, () => {
+		process.exit();
+	});
 });
 
 program.parse(process.argv);
--- packages/cli/index.js
+++ packages/cli/index.js
@@ -1,80 +1,111 @@
+const fs = require('fs');
+const ejs = require('ejs');
+const path = require('path');
 const http = require('http');
 const https = require('https');
 const { red, green } = require('chalk');
 const { Server } = require('@namics/remlog-server');
 const { URLS, getTraceUrl } = require('@namics/remlog-utils');
+const CMD_DIR = path.resolve(process.cwd());
+const TEMPLATE_PATH = path.resolve(__dirname, 'template');
+const TemplatePaths = {
+	Transport: path.resolve(TEMPLATE_PATH, 'Transport.ejs')
+};
 
-const start = (port = Server.defaultConfig.port, transport, cors = '*') => {
-    if (typeof cors === 'string') {
-        cors = cors.split(',').map(host => host.trim());
-    }
+const start = (port = Server.defaultConfig.port, transport, cors = '*', ssl) => {
+	if (typeof cors === 'string') {
+		cors = cors.split(',').map(host => host.trim());
+	}
 
-    const server = new Server({
-        port,
-        transport,
-        cors
-    });
+	const server = new Server({
+		port,
+		transport,
+		cors,
+		ssl
+	});
 
-    server.start();
+	server.start();
 
-    return server;
+	return server;
 };
 
 const request = (port = Server.defaultConfig.port, host = 'localhost', secure = false, payload) => {
-    const requestor = secure ? https : http;
-    let editablePayload;
+	const requestor = secure ? https : http;
+	let editablePayload;
 
-    try {
-        editablePayload = JSON.parse(payload);
-        editablePayload.file = '@namics/remlog-cli/bin/remlog-cli.js';
-        editablePayload.userAgent = `node ${process.cwd()}`;
-        editablePayload.line = 19;
-        editablePayload.client = 'CLI';
-        editablePayload.timestamp = new Date().toISOString();
-    } catch (e) {
-        return console.log(red(`Invalid JSON payload detected: ${e.message}`));
-    }
+	try {
+		editablePayload = JSON.parse(payload);
+		editablePayload.file = '@namics/remlog-cli/bin/remlog-cli.js';
+		editablePayload.userAgent = `node ${process.cwd()}`;
+		editablePayload.line = 19;
+		editablePayload.client = 'CLI';
+		editablePayload.timestamp = new Date().toISOString();
+	} catch (e) {
+		return console.log(red(`Invalid JSON payload detected: ${e.message}`));
+	}
 
-    const finalPayload = JSON.stringify(editablePayload);
+	const finalPayload = JSON.stringify(editablePayload);
 
-    const req = requestor.request(
-        {
-            port,
-            hostname: host,
-            path: URLS.TRACE,
-            method: 'POST',
-            headers: {
-                'X-RemLog-Client': 'CLI',
-                'Content-Type': 'application/json'
-            }
-        },
-        response => {
-            let data = '';
+	const req = requestor.request(
+		{
+			port,
+			hostname: host,
+			path: URLS.TRACE,
+			method: 'POST',
+			headers: {
+				'X-RemLog-Client': 'CLI',
+				'Content-Type': 'application/json'
+			}
+		},
+		response => {
+			let data = '';
 
-            response.setEncoding('utf8');
-            response.on('data', chunk => {
-                data += chunk;
-            });
-            response.on('end', () => {
-                const colorize = !!data.error ? red : green;
-                const finalData = JSON.parse(data);
+			response.setEncoding('utf8');
+			response.on('data', chunk => {
+				data += chunk;
+			});
+			response.on('end', () => {
+				const colorize = !!data.error ? red : green;
+				const finalData = JSON.parse(data);
 
-                console.log(
-                    colorize(`[${data.timestamp}] GET /logs/${data.id}.json (${data.error})`)
-                );
-            });
-        }
-    );
+				console.log(
+					colorize(`[${data.timestamp}] GET /logs/${data.id}.json (${data.error})`)
+				);
+			});
+		}
+	);
 
-    req.on('error', e => {
-        console.error(`${red(e.message)}`);
-    });
+	req.on('error', e => {
+		console.error(`${red(e.message)}`);
+	});
 
-    req.write(finalPayload);
-    req.end();
+	req.write(finalPayload);
+	req.end();
+};
+
+const copyTemplate = (sourcePath, dest, templateData) => {
+	return new Promise((resolve, reject) => {
+		fs.readFile(path.resolve(CMD_DIR, sourcePath), (err, data) => {
+			if (err) {
+				return reject(err);
+			}
+
+			const template = ejs.compile(data.toString(), {});
+
+			fs.writeFile(path.resolve(CMD_DIR, dest), template(templateData), err => {
+				if (err) {
+					return reject(err);
+				}
+
+				resolve();
+			});
+		});
+	});
 };
 
 exports = module.exports = {
-    start,
-    request
+	start,
+	request,
+	copyTemplate,
+	TemplatePaths
 };
