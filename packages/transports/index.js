514b88fdd9a7323027a47150bfb58214499edf51
Jan Biasi <jan.biasi@namics.com>
2018-01-24 13:36:07 +0100

    fix(transports): Fix main Transport export for extension
    BREAKING CHANGE: Transports weren't usable in Server mode

--- packages/transports/index.js
+++ packages/transports/index.js
@@ -1,34 +1,34 @@
 const ConsoleTransport = require('./Console');
 const FileSystemTransport = require('./FileSystem');
 const MongoDBTransport = require('./MongoDB');
-const Transport = require('./Transport');
+const Transport = require('./src/Transport');
 const pkg = require('./package.json');
 
 const getTransportById = (transportId = '') => {
-    if (typeof transportId !== 'string') {
-        return ConsoleTransport;
-    }
+	if (typeof transportId !== 'string') {
+		return ConsoleTransport;
+	}
 
-    switch (transportId) {
-        case ConsoleTransport.id:
-            return ConsoleTransport;
-        case FileSystemTransport.id:
-            return FileSystemTransport;
-        case MongoDBTransport.id:
-            return MongoDBTransport;
-        default:
-            return ConsoleTransport;
-    }
+	switch (transportId) {
+		case ConsoleTransport.id:
+			return ConsoleTransport;
+		case FileSystemTransport.id:
+			return FileSystemTransport;
+		case MongoDBTransport.id:
+			return MongoDBTransport;
+		default:
+			return ConsoleTransport;
+	}
 };
 
 const getTransportNameFromId = (transportId = '') => transportId.replace(`${pkg.name}/`, '');
 
 exports = module.exports = {
-    ConsoleTransport,
-    FileSystemTransport,
-    MongoDBTransport,
-    getTransportNameFromId,
-    getTransportById,
-    Transport,
-    GENERIC_TRANSPORT_LOGFILE: Transport.GENERIC_TRANSPORT_LOGFILE,
+	ConsoleTransport,
+	FileSystemTransport,
+	MongoDBTransport,
+	getTransportNameFromId,
+	getTransportById,
+	Transport,
+	GENERIC_TRANSPORT_LOGFILE: Transport.GENERIC_TRANSPORT_LOGFILE
 };
