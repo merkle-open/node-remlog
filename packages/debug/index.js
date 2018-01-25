bf2bfc1d5122e142bd9c07acc7247528641e39bf
Jan Biasi <jan.biasi@namics.com>
2018-01-25 10:36:42 +0100

    style(*): Update code styles

--- packages/debug/index.js
+++ packages/debug/index.js
@@ -1,34 +1,35 @@
 const { red, yellow, cyan, green, dim } = require('chalk');
 
 class Logger {
-    constructor(context) {
-        this.context = context;
-    }
+	constructor(context) {
+		this.context = context;
+	}
 
-    get contextPrefix() {
-        return `${dim(`[${this.context}]`)}`;
-    }
+	get contextPrefix() {
+		return `${dim(`[${this.context}]`)}`;
+	}
 
-    log(message) {
-        console.log(`${this.contextPrefix} ${message}`);
-    }
+	log(message) {
+		console.log(`${this.contextPrefix} ${message}`);
+	}
 
-    error(message) {
-        console.error(`${this.contextPrefix} ${red(message)}`);
-    }
+	error(message) {
+		console.error(`${this.contextPrefix} ${red(message)}`);
+	}
 
-    warn(message) {
-        console.warn(`${this.contextPrefix} ${yellow(message)}`);
-    }
-    info(message) {
-        console.info(`${this.contextPrefix} ${cyan(message)}`);
-    }
+	warn(message) {
+		console.warn(`${this.contextPrefix} ${yellow(message)}`);
+	}
 
-    success(message) {
-        console.log(`${this.contextPrefix} ${green(message)}`);
-    }
+	info(message) {
+		console.info(`${this.contextPrefix} ${cyan(message)}`);
+	}
+
+	success(message) {
+		console.log(`${this.contextPrefix} ${green(message)}`);
+	}
 }
 
 exports = module.exports = {
-    Logger,
+	Logger
 };
